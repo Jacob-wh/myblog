3cd72cd5971e78b4b691a47e858b042d02e2052c
wanghe <wanghe@socmap.net>
2018-05-19 20:03:32 +0800

    hhhhhhh

--- myblog/settings.py
+++ myblog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -24,9 +23,8 @@ SECRET_KEY = 'f#a_w-2ur#&$w++!gb&mkx@hgd5c-vdk5h49rxju+0jg0cj5%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
@@ -56,7 +54,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR , "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +69,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myblog.wsgi.application'
 
-LANGUAGE_CODE = 'zh-hans'  #LANGUAGE_CODE = 'zh-hans'
+LANGUAGE_CODE = 'zh-hans'  # LANGUAGE_CODE = 'zh-hans'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -116,11 +112,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static/')
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__),'../blog/static/'),
+)
 
